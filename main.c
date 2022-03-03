fd721863f00595a5eae8f60235d064e4f848c0c4
Joogi <100830752+jakubzie@users.noreply.github.com>
2022-03-03 08:25:25 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define pi 3.14
+int main(void) {
+  int r=1;
+  double Pkola=r*r*pi;
+  printf("Pole koła jest równe: %f\n",Pkola);
+  int a,b,h;
+  // dowiedzieć się na czym polega scanf, przetestować jej użycie (wprowadzanie danych do programu), zrobić 1.1, 1.2, 1.3 lub więcej zadań, zaktywować konto pro na githubie
+  return 0;
+}
