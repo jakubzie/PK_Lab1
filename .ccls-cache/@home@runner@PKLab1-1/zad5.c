0a40c3ca69fefc0d42edcf3f59886c78ce297299
Joogi <100830752+jakubzie@users.noreply.github.com>
2022-03-10 08:07:47 +0000

    ...

--- .ccls-cache/@home@runner@PKLab1-1/zad5.c
+++ .ccls-cache/@home@runner@PKLab1-1/zad5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <complex.h>
 
 int main(){
   double a,b,c,delta,x0,x1,x2;
@@ -7,7 +8,12 @@ int main(){
   scanf("%lf" "%lf" "%lf", &a, &b, &c);
   delta = pow(b,2)-4*a*c;
   if(delta<0){
-    printf("Delta ujemna");
+    double complex z1,z2,pd;
+    printf("%lf",delta);
+    pd = pow(b*b-4*a*c,0.5);
+    z1 = (-b+pd)/(2*a);
+    z2 = (-b-pd)/(2*a);
+    printf("Delta ujemna. Pierwiastki urojone rownania: z1= %lf %lf, z2= %lf %lf", creal(z1), cimag(z1), creal(z2), cimag(z2));
   }
   else if(delta>0){
     x1 = (-b+pow(delta,0.5))/(2*a);
